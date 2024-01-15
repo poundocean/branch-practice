a13a589eebc920a3f16ef78fc6d535ea2ee4b064
poundocean <ji2037070@gmail.com>
2024-01-15 14:07:35 +0900

    feat: Print fizz if number is multiple of 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,10 @@
-print("fizzbuzz")
+def fizz():
+    for i in range(1,15+1):
+        if i%3==0:
+            print('fizz')
+        else:
+            print(f'{i}')
+
+
+if __name__=='__main__':
+    fizz()
