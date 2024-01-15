b9e9742852fce545b9c1196ae2226acedd4b530b
poundocean <ji2037070@gmail.com>
2024-01-15 14:30:15 +0900

    feat: refactor fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,10 @@
-def fizzbuzz():
+def do_fizzbuzz():
     for i in range(1,15+1):
-        if i%5==0 and i%3==0:
-            print('fizzbuzz')
-        elif i%3==0:
-            print('fizz')
-        elif i%5==0:
-            print('buzz')
+        if i%3==0 or i%5==0:
+            print('fizz'*(i%3==0)+'buzz'*(i%5==0))
         else:
             print(f'{i}')
 
 
 if __name__=='__main__':
-    fizzbuzz()
+    do_fizzbuzz()
